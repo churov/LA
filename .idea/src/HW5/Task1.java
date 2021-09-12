02d2b416077faa429d7f1e2b89232471f959fd05
Artsiom Churau <churovaa@yandex.by>
2021-09-12 12:19:23 +0300

    Start

---
+++ .idea/src/HW5/Task1.java
@@ -0,0 +1,13 @@
+package HW5;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+        int sum = 0;
+        for (int i = 0; i<array.length; i++){
+            sum = sum + array[i];
+            System.out.println(sum);
+        }
+        System.out.println(sum);
+    }
+}
