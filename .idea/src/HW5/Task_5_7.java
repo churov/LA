5df5fd5625db2b7bbe3afb718ac1aab16d509358
churov <churovaa@yandex.by>
2021-09-13 09:34:35 +0300

    And7Task

---
+++ .idea/src/HW5/Task_5_7.java
@@ -0,0 +1,17 @@
+public class Task_5_7 {
+    public static void main(String[] args) {
+        int[][] array = {
+                {1, 2, 3, 4, 5},
+                {6, 7, 8, 9},
+                {-1, -2, -3, -4, -5},
+                {-5, -6}};
+        int count = 0;
+        for (int i =0; i<array.length; i++) {
+            for (int j = 0; j<array.length; j++){
+                count++;
+            }
+
+        }
+        System.out.println(count);
+    }
+}
